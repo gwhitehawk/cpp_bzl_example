8ae184ed26c3b0ae606ef52b9ffaca5e13411c0d
Miroslava Sotakova <miroslavasotakova@MiroslaasLaptop.telekom.ip>
2025-07-10 18:05:18 +0200

    uncomment test

--- lib/letter_sorter_test.cc
+++ lib/letter_sorter_test.cc
@@ -1,11 +1,10 @@
 #include <algorithm>
 #include "letter_sorter.h"
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
 namespace letter_sorter {
-// TEST(letter_sorter_test, basic_test) {
-//     LetterSorter sorter("Mirka");
-//     for(int i=0; i<sz; i++)
-//         EXPECT_EQ(sorter.GetSortedString(), "aikMr");
-// }
+TEST(letter_sorter_test, basic_test) {
+  LetterSorter sorter("Mirka");
+  EXPECT_EQ(sorter.GetSortedString(), "aikMr");
+}
 }  // namespace letter_sorter
